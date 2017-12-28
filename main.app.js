88fd7ff8236ae66118eb60d18febd3ff9b0c3743
Lena Madenci <lenamadenci@gmail.com>
2017-12-27 22:15:30 -0500

    loaded data and created seat directive

--- main.app.js
+++ main.app.js
@@ -1,9 +1,93 @@
 var app = angular.module('lolaSeats', []);
 
-app.controller('RootController', ['$scope', function($scope)
+// create a seat directive with all info
+app.directive("seatSpecs", function(){
+return {
+  restrict: 'A',
+  templateUrl: 'seat-specs.html'
+       };
+});
+
+app.controller('RootController', ['$scope', '$http', function($scope, $http)
 {
   var self = this;
-  self.hi= "so";
+
+  // load data
+  self.seatList = [];
+  $http.get('data/seats.json')
+
+  .success(function(data) {
+    // add each seat in data to list
+    for (seat in data) {
+      self.seatList.push(data[seat]);
+    }
+  })
+  .error(function(err) {
+    console.error('Repos error', err);
+  });
+
+  console.log(self.seatList);
 
 
 }]);
+
+
+
+
+
+
+
+/*
+// Task:
+// Seats have a cabin class (first, business, coach),
+// a row (1, 2 etc.), and a letter assignment. They can
+// be occupied, available or selected (only 1 should be
+// selectable at a time - in the wireframe, that's the
+// green one). The aisle in a cabin class is determined
+// by a missing letter in the seats (i.e. a, b, d, e - ‘c’
+// is missing, that’s where the aisle is). Cabin classes
+// should be programmatically generated to support any
+// number of rows / columns.
+*/
+
+
+// 1. Organize data -- do we need to do this separately or just map?
+// 2. visually indicate states of seats to data
+// 3. all user to select
+
+// map the seats --- need to go thru data to determine number of rows and columns
+
+// show all seats (as boxes)
+// // if occupied, display as unselectable (not included in list we are working with)
+// // if available, display with 2 states: normal, and 0 or 1 seats selected
+
+
+/*
+{
+    "class": "Economy", // First // Business
+    "seat": "A", // - J
+    "row": 39,
+    "occupied": true,
+    "premium": false,
+    "overWing": false
+},
+
+
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+ a b / d e
+
+
+
+*/
